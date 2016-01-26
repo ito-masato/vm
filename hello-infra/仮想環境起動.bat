19f8e5d04e31b5b008edbcdb1089f1a15a013a64
ito-masato <ito-masato@plazacreate.co.jp>
2016-01-26 16:57:36 +0900

    bat

---
+++ hello-infra/仮想環境起動.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem ----------------------------------------------------------
+rem Copyright (c) PlazaCreate Co.,Ltd. 
+rem 仮想OS(Ubuntu)をteratermで起動する
+rem Written by M.Ito 2016.01.26
+rem ----------------------------------------------------------
+
+rem ディレクトリ移動
+cd C:\Git\vm\hello-infra
+
+rem vagrantからUbuntuを起動
+vagrant teraterm
